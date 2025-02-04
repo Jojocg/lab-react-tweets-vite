84f9d7e6f13867960d9341ff0492d1b91943da2c
Johana Castellano <johanacg16@gmail.com>
2025-02-04 17:43:29 +0000

    iteration 4

---
+++ src/components/ProfileImage.jsx
@@ -0,0 +1,9 @@
+export default function ProfileImage(props) {
+    /* console.log(props); */
+    return (
+        <>
+            <img src={props.image} className="profile" alt="profile" />
+        </>
+    );
+}
+
